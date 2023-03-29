3f19809872758f77cd9503cc170f3b00d1a37279
jh950925 <49939169+jh950925@users.noreply.github.com>
2023-03-29 15:01:11 +0900

    [Bronze V] Title: A+B - 7, Time: 152 ms, Memory: 16084 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= count; i++) {
+            StringTokenizer tk = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(tk.nextToken());
+            int b = Integer.parseInt(tk.nextToken());
+            bw.write("Case #" + i + ": " + (a + b) + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }//end main
+}//end class
