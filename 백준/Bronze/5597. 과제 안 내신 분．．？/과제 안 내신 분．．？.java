e47d211bcf50a113bff16eaaffdceecca6196dfd
jh950925 <49939169+jh950925@users.noreply.github.com>
2023-04-13 16:07:05 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 220 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> total = new ArrayList<>();
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i = 1; i <= 30; i++) {
+            total.add(i);
+        }
+        for (int i = 0; i < 28; i++) {
+            arr.add(sc.nextInt());
+        }
+        Collections.sort(arr);
+
+        total.removeAll(arr);
+
+        for (Integer n : total) {
+            System.out.println(n);
+        }
+
+    }//end main
+}//end class
